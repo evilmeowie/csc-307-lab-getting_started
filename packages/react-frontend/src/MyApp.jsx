cedb16a60593d430bad7458c17ba736067249582
evilmeowie <dashabaitazarova@gmail.com>
2026-04-07 18:26:29 -0700

    Completed up to part 9 IA:1

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,11 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+// src/MyApp.jsx (Adding the form component after the table)
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-     {
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        name: "Dennis",
-        job: "Bartender"
-      }
-  ]);
-
+    const [characters, setCharacters] = useState([]);
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -34,8 +18,8 @@ function MyApp() {
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
+      <Form />
     </div>
   );
 }
-
 export default MyApp;
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -23,7 +23,6 @@ function TableHeader() {
         </td>
       </tr>
       
-        
       );
      }
     );
