2118c40ac31c48cd295fa688d1654eea84825352
evilmeowie <dashabaitazarova@gmail.com>
2026-04-07 20:20:49 -0700

    Final submission. All parts completed

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,24 +1,29 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
-// src/MyApp.jsx (Adding the form component after the table)
 import Form from "./Form";
 
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
     setCharacters(updated);
   }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
       <Table
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
-      <Form />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
